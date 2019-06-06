50c638d54378b41e013d2103bef2351b0a5b610b
kirk <kzimmer97@github.com>
2019-06-06 03:34:17 -0400

    added decimals

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # K
 I'm making a language!
+
+Lexeme List
+
+identifier : [A-Za-z][A-za-z0-9]*
+separator : ( | )
+literal integer : [0-9]* 
+literal string : "anything in quotes"
+literal decimal : [0-9][.][0-9]* | [.][0-9][0-9]*
+
+```
+file.txt => 
+0.42 42 .42 42. "42"
+    (    forty2        4two)
+```
+
+```
+$ go run *.go file.txt
+```
+```
+0.42 - literal decimal
+42 - literal integer
+.42 - literal decimal
+42. - literal decimal
+"42" - literal string
+( - separator
+forty2 - identifier
+4 - literal integer
+two - identifier
+) - separator
+```
--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
 	"log"
 	"os"
 )
 
 const whiteSpace = 32
 const doubleQuotes = 34
+const dot = 46
 
 func main() {
 	parseMap, err := parseFile(os.Args[1])
@@ -31,10 +34,18 @@ func parseFile(fileName string) (*ParseMap, error) {
 	var tokens []string
 	var lexemes []string
 
+	lineCount := 1
+
 	for scanner.Scan() {
-		t, l := parseLine(scanner.Text())
+		t, l, err := parseLine(scanner.Text(), lineCount)
+
+		if err != nil {
+			return nil, err
+		}
+
 		tokens = append(tokens, t...)
 		lexemes = append(lexemes, l...)
+		lineCount++
 	}
 
 	lexMap := make(map[string]string)
@@ -51,7 +62,7 @@ func parseFile(fileName string) (*ParseMap, error) {
 	return &pm, nil
 }
 
-func parseLine(str string) ([]string, []string) {
+func parseLine(str string, line int) ([]string, []string, error) {
 	//standardize whitespacing
 	runes := []rune(str)
 	var r int
@@ -65,7 +76,7 @@ func parseLine(str string) ([]string, []string) {
 
 	for i < len(runes) {
 		r = int(runes[i])
-		//state looking => we don't have a start index yet
+		//state blank => we don't have a start index yet
 		if state == "blank" {
 			if r != whiteSpace {
 				if isParenthesis(r) {
@@ -82,13 +93,20 @@ func parseLine(str string) ([]string, []string) {
 				} else if isNumber(r) {
 					start = i
 					end = -1
-					lexeme = "literal"
+					lexeme = "literal integer"
 					state = "number"
 				} else if r == doubleQuotes {
 					start = i
 					end = -1
-					lexeme = "literal"
+					lexeme = "literal string"
 					state = "inQuotes"
+				} else if r == dot {
+					start = i
+					end = -1
+					lexeme = "literal decimal"
+					state = "decimal"
+				} else {
+					return nil, nil, parseError(line)
 				}
 			}
 		} else if state == "letter" { //start index maps to a letter
@@ -101,12 +119,32 @@ func parseLine(str string) ([]string, []string) {
 			} else {
 			}
 		} else if state == "number" { //only numbers can be tokenized at this point
-			if !isNumber(r) {
+			if r == dot {
+				state = "decimal"
+				lexeme = "literal decimal"
+				i++
+				continue
+			} else if !isNumber(r) {
 				end = i
 				words = append(words, string(runes[start:end]))
 				lexemes = append(lexemes, lexeme)
 				state = "blank"
 				continue
+			}
+		} else if state == "decimal" {
+			if r == dot {
+				return nil, nil, parseError(line)
+			}
+			if !isNumber(r) {
+				end = i
+
+				if end == start+1 { //this invalidates a . decimal
+					return nil, nil, parseError(line)
+				}
+
+				words = append(words, string(runes[start:end]))
+				lexemes = append(lexemes, lexeme)
+				state = "blank"
 			}
 		} else if state == "inQuotes" { //handles quoted strings
 			if r == doubleQuotes {
@@ -119,13 +157,17 @@ func parseLine(str string) ([]string, []string) {
 
 		i++
 	}
-	//to append the very last word
+	//to handle the very last word
 	if end == -1 {
+		if lexeme == "literal decimal" && start+1 == i { //this invalidates a . decimal
+			return nil, nil, parseError(line)
+		}
+
 		words = append(words, string(runes[start:i]))
 		lexemes = append(lexemes, lexeme)
 	}
 
-	return words, lexemes
+	return words, lexemes, nil
 }
 
 func isNumber(ascii int) bool {
@@ -138,4 +180,8 @@ func isLetter(ascii int) bool {
 
 func isParenthesis(ascii int) bool {
 	return ascii == 40 || ascii == 41
+}
+
+func parseError(line int) error {
+	return errors.New(fmt.Sprintf("Bad Parse on line: %d\n", line))
 }
